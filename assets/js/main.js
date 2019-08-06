ea3b5437295c94e6bb48691747aa180f2418ea42
Sarthak Sachdeva <sarthaksac7@gmail.com>
2019-08-06 13:11:22 +1000

    test

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+    $('.carousel').carousel('pause');
+    
+    
+});
+
+
+$(document).ready(function() {
+    console.log('read');
+    window.scrollTo(0,9999);
+});
